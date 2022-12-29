75c0c9cb5cbeaba0f26270ff49d5572aebbdab33
beomtae <95863810+beomtae@users.noreply.github.com>
2022-12-29 13:39:33 +0900

    POST 메소드 사용 & 가짜 DB로 작동확인

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,52 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => {
-  res.render("watch");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 0,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  res.render("edit");
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  res.send("Delete");
+export const getEdit = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  videos[id].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  //add video to the videos array.
+  return res.redirect("/");
 };
